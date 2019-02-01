eceae95c4c08549e4300c267b7c2e675b831b4c7
Luke Stokes <luke.stokes@gmail.com>
2019-02-01 16:11:21 -0400

    Replacing TestDAC with VideoDAC

--- eosdac-material/src/pages/credits.vue
+++ eosdac-material/src/pages/credits.vue
@@ -41,7 +41,7 @@
       [ {{others.join(', ')}}, ... ]
     </div>
 
-    <div class="retrotext relative-position full-width q-mt-xl" style="font-size:20px; text-align:right">© TestDAC</div>
+    <div class="retrotext relative-position full-width q-mt-xl" style="font-size:20px; text-align:right">© VideoDAC</div>
 
   </div>
   <!-- -- -->
@@ -58,7 +58,7 @@
     </div>
 
     <div class=" q-pa-lg relative-position round-borders bg-dark2 shadow-5 q-mt-md">
-      <div class="q-mb-md">TestDAC Workers</div>
+      <div class="q-mb-md">VideoDAC Workers</div>
       <div class="text-dimwhite">{{dac_workers.join(', ')}}</div>
     </div>
 
